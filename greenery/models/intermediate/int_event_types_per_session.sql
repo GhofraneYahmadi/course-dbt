867907f131af95d7cc4db3af450791729d1befbd
Ghofrane Yahmadi <ghofrane.yahmadi@audibene.de>
2023-01-29 23:10:51 +0000

    update

--- greenery/models/intermediate/int_event_types_per_session.sql
+++ greenery/models/intermediate/int_event_types_per_session.sql
@@ -5,17 +5,18 @@ with events as (
 )
 
 {%- set event_types = dbt_utils.get_column_values(
-      table=ref('stg_postgres_events'), 
-      column='event_type') 
+      table=ref('stg_postgres_events') 
+      ,column='event_type'
+      ,order_by='event_type asc'
+      ) 
 -%}
 
 select 
     session_id
-    , user_id
     , product_id
     , order_id
     {%- for event_type in event_types %}
     , {{aggregation('event_type', event_type)}} as {{event_type}}
     {%- endfor %}
 from events
-group by 1,2,3,4
+group by 1,2,3
